c76a8fb668e688b52721b6b8d2358fa4b119fb78
rizwanfarooq07 <rizwanfarooqui07.com>
2022-06-23 12:13:30 +0530

    updated projects

--- data.ts
+++ data.ts
@@ -110,8 +110,19 @@ export const hobbies: SkillsInterface[] = [
 ];
 
 export const projects: ProjectsInterface[] = [
-  {
+    {
     id: 1,
+    name: "Spotify 2.0",
+    description:
+      "A spotify demo app that allows user to login with their spotify account browse through their favourite playlists and select different songs. Used spotiy OAuth to authenticate the users.",
+    image_path: "/images/spotify.png",
+    deployed_url: "https://spotify2-0-three.vercel.app/",
+    github_url: "https://github.com/rizwanfarooq07/spotify2.0",
+    category: ["next"],
+    key_techs: ["NextJS", "Javascript", "TailwindCSS", "Recoil"],
+  },
+  {
+    id: 2,
     name: "ProShop",
     description:
       "A full stack shopping application that allows user to create their personal account, add items to cart and checkout after filling all the shipping details.",
@@ -122,7 +133,29 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "Bootstrap", "Nodejs", "Redux"],
   },
   {
-    id: 2,
+    id: 3,
+    name: "Tell the Sky",
+    description:
+      "A weather application that informs the users about the current weather of any city of the user's choice.",
+    image_path: "/images/tellthesky.png",
+    deployed_url: "https://tell-the-sky.netlify.app",
+    github_url: "https://github.com/rizwanfarooq07/tll-the-sky",
+    category: ["react"],
+    key_techs: ["React", "Javascript", "TailwindCSS", "Openweatherapi"],
+  },
+  {
+    id: 4,
+    name: "Google 2.0",
+    description:
+      "A demo google application with the google search (all , news, images , videos ) functionality.",
+    image_path: "/images/google.png",
+    deployed_url: "https://sleek-search.netlify.app/search",
+    github_url: "https://github.com/rizwanfarooq07/google_search",
+    category: ["react"],
+    key_techs: ["React", "Javascript", "TailwindCSS", "RapidApi"],
+  },
+  {
+    id: 5,
     name: "Hulu 2.0",
     description:
       "A demo hulu application that let's you browse through different categories of movies that are fetched using TMDb api.",
@@ -133,7 +166,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["NextJS", "Typescript", "TailwindCSS"],
   },
   {
-    id: 3,
+    id: 6,
     name: "Realtor",
     description:
       "A real estate application that let's you browse through proprties available for rent/buy in and around UAE. You can apply different filters like rooms, locations, price, area etc. in order to find the property that suits you the most.",
@@ -144,7 +177,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["NextJS", "Javascript", "Chakra-Ui", "RapidApi"],
   },
   {
-    id: 4,
+    id: 7,
     name: "Finding Queen",
     description:
       "King Shan has received intelligence that Al Falcone is hiding in one of six neighbouring planets. In this problem you need to build a UI through which King Shan can choose the planets to search, and the vehicles to use in Finding Falcone.",
@@ -155,7 +188,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "CSS"],
   },
   {
-    id: 5,
+    id: 8,
     name: "GetFit",
     description:
       "An exercise browser application built with an aim to help you achieve your fitness goals. It allows user to browse through hundreds of different exercises avaiilable related to any body part, equipments etc",
@@ -166,18 +199,18 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "Javascript", "Material UI", "RapidApi"],
   },
   {
-    id: 6,
+    id: 9,
     name: "TeeRexStore",
     description:
       "A front-end teeshirts shopping store that let's you search, filter(price, color, gender, type) and add products to cart then go to cart to see the order details like total amount, quantity etc.",
     image_path: "/images/teerex.png",
-    deployed_url: "https://get-exercised.netlify.app",
+    deployed_url: "https://teerexstore.netlify.app",
     github_url: "https://github.com/rizwanfarooq07/teerexstore",
     category: ["react"],
     key_techs: ["React", "Material UI", "React-router V6"],
   },
   {
-    id: 7,
+    id: 10,
     name: "CineBucket",
     description:
       "A frontend movie and shows browsing application that allows users to search shows, movies or both simultaneously using OMDb api",
@@ -188,7 +221,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "SCSS", "Redux-toolkit"],
   },
   {
-    id: 8,
+    id: 11,
     name: "Facebook Clone App",
     description:
       "A facebook clone application that let's users to login using their google account and post pictures with cations",
@@ -199,7 +232,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "TailwindCSS", "Google Firebase"],
   },
   {
-    id: 9,
+    id: 12,
     name: "MeetUps",
     description:
       "A full stack application that allows users to post thier meeting place in order to keep their precious memories safe.",
@@ -211,7 +244,7 @@ export const projects: ProjectsInterface[] = [
     key_techs: ["React", "Nodejs", "CSS"],
   },
   {
-    id: 10,
+    id: 13,
     name: "Quotes for Thought",
     description:
       "A full stack application that allows users to convert thier thoughts into valuable quotes",
